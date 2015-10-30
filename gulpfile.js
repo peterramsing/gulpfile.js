171190184db5f2aad7ec2aaddf9f55c4e7d30daa
Jay Pick <jaypick.me@gmail.com>
2015-10-30 11:46:36 +0000

    [added gulp-notify, gulp-rename, gulp-autoprefixer + switch gulp-ruby-sass for gulp-sass]

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
-var gulp   = require('gulp'),
-	uglify = require('gulp-uglify'),
-	sass   = require('gulp-ruby-sass');
+var gulp         = require('gulp'),
+	uglify       = require('gulp-uglify'),
+	sass         = require('gulp-sass'),
+	rename       = require('gulp-rename'),
+	notify       = require('gulp-notify'),
+	autoprefixer = require('gulp-autoprefixer');
+
+var watch_paths = {
+	scripts: ['assets/js/libs/*.js'],
+	styles:  ['assets/css/scss/**/*.scss']
+};
 
 function errorLog(error) {
 	console.error.bind(error);
@@ -9,26 +17,32 @@ function errorLog(error) {
 
 // Scripts Task
 gulp.task('scripts', function(){
-	gulp.src('js/*.js')
-		.pipe(uglify())
-		.on('error', errorLog)
-		.pipe(gulp.dest('js/min'));
+	gulp.src(watch_paths.scripts)
+	.pipe(rename({
+        suffix: '.min'
+    }))
+	.pipe(uglify())
+	.on('error', errorLog)
+	.pipe(gulp.dest('assets/js'))
+	.pipe(notify({ message: 'Scripts task complete' }));
 });
 
 // Styles Task
 gulp.task('styles', function(){
-	gulp.task('scss/**/*.scss')
-		.pipe(sass({
-			style: 'compressed'
-		}))
-		.on('error', errorLog)
-	.pipe(gulp.dest('css/'));
+	gulp.src(watch_paths.styles)
+	.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+	.pipe(sass({
+		outputStyle: 'compressed'
+	}))
+	.on('error', errorLog)
+	.pipe(gulp.dest('assets/css'))
+	.pipe(notify({ message: 'Styles task complete' }));
 });
 
 // Watch Task
 gulp.task('watch', function(){
-	gulp.watch('js/*.js', ['scripts']);
-	gulp.watch('scss/**/*.scss', ['styles']);
+	gulp.watch(watch_paths.scripts, ['scripts']);
+	gulp.watch(watch_paths.styles, ['styles']);
 });
 
 gulp.task('default', ['scripts', 'styles', 'watch']);
